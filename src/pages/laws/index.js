0116c6b0eab6ff827ebb2a38e9a2c0f3c89fbeb6
mundi morgado <mundi@tandemseven.com>
2016-09-18 11:51:08 -0700

    update

--- src/pages/laws/index.js
+++ src/pages/laws/index.js
@@ -25,19 +25,20 @@ class Laws extends Component {
         <h2 className='h2'>What's Happening In Your State?</h2>
         <div style={{padding: '0 1em'}}>
           <h3>U.S. State Laws on the Private Ownership of Primates:</h3>
-          <h4>Primate Incidents 1990 - 2015</h4>
+          <h4>Primate Incidents 1990 - 2016</h4>
           <Map onClick={this.clickState} states={states} />
           <div>
             <dl>
-              <dt style={{backgroundColor: $orange}}>&nbsp;</dt>
+              <dt style={{backgroundColor: $blue}}>&nbsp;</dt>
               <dd>No Laws</dd>
               <dt style={{backgroundColor: $yellow}}>&nbsp;</dt>
-              <dd>Requires License or Permit to Own</dd>
-              <dt style={{backgroundColor: $blue}}>&nbsp;</dt>
-              <dd>Partial Ban <small>(ban on certain species of primates)</small></dd>
+              <dd>License or Permit Required</dd>
+              <dt style={{backgroundColor: $orange}}>&nbsp;</dt>
+              <dd>Ban on Some Primates Species</dd>
               <dt style={{backgroundColor: $green}}>&nbsp;</dt>
               <dd>Ban on all Primate Species</dd>
             </dl>
+
           </div>
           <br />
           <br />
